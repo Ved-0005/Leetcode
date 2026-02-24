c282a7925d0a63be31cc774970757812e8f1d5cc
Ved-0005 <vedverma292@gmail.com>
2026-02-24 15:32:00 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (22.99%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,29 +3,40 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        vector<pair<int,int>>indexes;
+        bool r1=1,c1=1;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0)
-                indexes.push_back({i,j});
+            if(matrix[i][0]==0){
+                c1=0;
+                break;
             }
         }
-        vector<bool>vrow(m,0);
-        vector<bool>vcol(n,0);
-        for(int i=0;i<indexes.size();i++){
-            int row=indexes[i].first;
-            int col=indexes[i].second;
-            if(vrow[row]==0){
-                for(int j=0;j<n;j++){
-                    matrix[row][j]=0;
-                }
-                vrow[row]=1;
+        for(int i=0;i<n;i++){
+            if(matrix[0][i]==0){
+                r1=0;
+                break;
             }
-            if(vcol[col]==0){
-                for(int j=0;j<m;j++){
-                    matrix[j][col]=0;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
-                vcol[col]=1;
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0||matrix[0][j]==0)
+                matrix[i][j]=0;
+            }
+        }
+        if(r1==0){
+            for(int i=0;i<n;i++)
+            matrix[0][i]=0;
+        }
+        if(c1==0){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
             }
         }
     }
