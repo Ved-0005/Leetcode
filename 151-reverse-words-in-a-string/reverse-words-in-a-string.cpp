1029a396e5295025f35fdd30954fabeef7523ee1
Ved-0005 <vedverma292@gmail.com>
2026-07-09 19:19:56 +0530

    Time: 2 ms (41.48%) | Memory: 10.7 MB (42.45%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans;
+        stringstream ss(s);
+        string word;
+        stack<string>st;
+        while(ss>>word){
+            st.push(word);
+        }
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+            if(!st.empty()) ans+=' ';
+        }
+
+        return ans;
+    }
+};
